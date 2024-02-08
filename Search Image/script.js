3068c01402137c4443a91a86cbecd8ebe3d10dcc
evinsdesign <kelvinsaputra024@gmail.com>
2024-02-08 16:20:24 +0800

    create feature linier gradient

--- Search Image/script.js
+++ Search Image/script.js
@@ -9,11 +9,44 @@ let page = 1;
 
 async function searchImage() {
   keyword = searchInput.value;
-  const urlApi = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const urlApi = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
   const response = await fetch(urlApi);
   const data = await response.json();
+
   console.log(data);
+  const results = data.results;
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    const gradient = document.createElement("div");
+    gradient.setAttribute("class", "gradient");
+
+    const profile = document.createElement("div");
+    profile.setAttribute("class", "profile");
+
+    const imgProfil = document.createElement("img");
+    imgProfil.src = result.user.profile_image.large;
+
+    const heading2 = document.createElement("h2");
+    heading2.innerHTML = result.user.name;
+
+    const socialMedia = document.createElement("p");
+    socialMedia.innerHTML = "@" + result.user.social.instagram_username;
+
+    profile.appendChild(heading2);
+    profile.appendChild(socialMedia);
+    profile.appendChild(imgProfil);
+    gradient.appendChild(profile);
+    imageLink.appendChild(gradient);
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
 }
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
